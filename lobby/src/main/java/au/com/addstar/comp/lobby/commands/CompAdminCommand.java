05f8f01644790af15a8f946f6a6cdf61342ecd31
mylesk <11828103+myleskeeffe@users.noreply.github.com>
2026-01-14 09:38:52 +0800

    feat: Register commands

--- lobby/src/main/java/au/com/addstar/comp/lobby/commands/CompAdminCommand.java
+++ lobby/src/main/java/au/com/addstar/comp/lobby/commands/CompAdminCommand.java
@@ -1,7 +1,10 @@
 package au.com.addstar.comp.lobby.commands;
 
+import au.com.addstar.comp.confirmations.ConfirmationManager;
 import au.com.addstar.comp.lobby.CompManager;
 import au.com.addstar.comp.lobby.commands.signs.SignCommand;
+import au.com.addstar.comp.lobby.services.CompetitionJoinService;
+import au.com.addstar.comp.lobby.services.CompetitionViewService;
 import au.com.addstar.comp.lobby.signs.SignManager;
 import au.com.addstar.comp.redis.RedisManager;
 import au.com.addstar.comp.util.Messages;
@@ -9,8 +12,12 @@ import au.com.addstar.comp.whitelist.WhitelistHandler;
 import au.com.addstar.monolith.command.RootCommandDispatcher;
 
 public final class CompAdminCommand extends RootCommandDispatcher {
-	public CompAdminCommand(WhitelistHandler whitelist, CompManager manager, RedisManager redis, SignManager signManager, Messages messages) {
+	public CompAdminCommand(WhitelistHandler whitelist, CompManager manager, RedisManager redis, SignManager signManager, Messages messages, ConfirmationManager confirmationManager) {
 		super("Gives access to all comp administration commands");
+		
+		// Create services for commands
+		CompetitionJoinService joinService = new CompetitionJoinService(confirmationManager, messages);
+		CompetitionViewService viewService = new CompetitionViewService(messages);
 		
 		registerCommand(new WhitelistCommand(whitelist));
 		registerCommand(new ListCommand(manager));
@@ -19,5 +26,7 @@ public final class CompAdminCommand extends RootCommandDispatcher {
 		registerCommand(new SignCommand(signManager, manager));
 		registerCommand(new InfoCommand(manager, messages));
 		registerCommand(new StateCommand(manager, messages));
+		registerCommand(new JoinCommand(manager, joinService, messages));
+		registerCommand(new ViewCommand(manager, viewService, messages));
 	}
 }
